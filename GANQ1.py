98a84773c8f5ff58f0ba3a73a5a2cd854af4a2b7
mennaML <mennaML>
2019-04-15 18:39:56 -0400

    Q1.3 working

--- GANQ1.py
+++ GANQ1.py
@@ -20,17 +20,21 @@ def KL(p, q):
 def JS(p, q):
     return 0.5*(KL(p, (p+q)/2) + KL(p, (p+q)/2))
 
-def JSD_loss_function(Dx, Dy, p, q):
-    #E_pDx = torch.mean(torch.log(Dx))
-    #E_qDy = torch.mean(torch.log(1 - Dy))
-    
-    return -1*(np.log(2) + 0.5*torch.mean(torch.log(Dx)) + 0.5*torch.mean(torch.log(1 - Dy)))
+def JSD_loss_function(Dx, Dy, p=0, q=0):
+    return -1*(0.5*torch.mean(torch.log(Dx)) + 0.5*torch.mean(torch.log(1 - Dy)))
 
-def W_loss_function(Tx, Ty, p, q):
-    E_pTx = np.mean(np.where(p != 0, p * Tx, 0))
-    E_qTy = np.mean(np.where(q != 0, q * Ty, 0))
+def WD_loss_function(Tx, Ty, p, q):
+    #E_pTx = np.mean(np.where(p != 0, p * Tx, 0))
+    #E_qTy = np.mean(np.where(q != 0, q * Ty, 0))
 
-    return -1*(E_pTx - E_qTy)
+    return -1*(np.mean(Tx) - np.mean(Ty))
+
+###############################################################################
+#
+# Discriminator Class
+#
+###############################################################################
+
 class Discriminator(nn.Module):
     def __init__(self, input_size, hidden_size):
         super(Discriminator, self).__init__()
@@ -49,18 +53,13 @@ class Discriminator(nn.Module):
             torch.nn.init.xavier_uniform(m.bias.data)
     
     def forward(self, x):
-        # flatten input if needed
-        x = torch.cat((x[:,0], x[:,1]), 0)
-        #x.view(512*2)
-        #print(x)
         x = self.layers(x)
         return x
 
 
 
-def train(model, loss_fn, num_epochs, batch_size):
-    phi = 0.1
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+def train(model, loss_fn, num_epochs, batch_size, phi, lmdba=10):
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.005)
     p_generator = sampler.distribution1(0, batch_size=batch_size)
     q_generator = sampler.distribution1(phi, batch_size=batch_size)
 
@@ -76,10 +75,11 @@ def train(model, loss_fn, num_epochs, batch_size):
         Dy = model(Variable(q))
         loss = loss_fn(Dx, Dy, p, q)
 
-        if loss_fn == W_loss_function:
+        if loss_fn == WD_loss_function:
+            #sample a and compute z
             Dz = model(Variable(p))
             loss.backward(retain_graph=True, create_graph=True)
-            grad_Tz = torch.autograd.grad(loss, Dz, retain_graph=True)
+            grad_Tz = torch.autograd.grad(Dz, z, retain_graph=True)
             loss += torch.norm(grad_Tz, p=2, dim=-1)
             print('agg grad')
         
@@ -92,10 +92,53 @@ def train(model, loss_fn, num_epochs, batch_size):
             print( "Epoch %6d. Loss %5.3f" % ( epoch+1, loss ) )
         
     print( "Training complete" )
+    return model
         
 
 
+def eval(model, batch_size, phi):
 
-D = Discriminator(input_size=512*2, hidden_size=64)
-#D.apply(weights_init)
-train(model=D, loss_fn=JSD_loss_function, num_epochs=100, batch_size=512)
+    #p_generator = sampler.distribution1(0, batch_size=batch_size)
+    q_generator = sampler.distribution1(phi, batch_size=batch_size)
+
+    model.eval()
+        
+    #p = torch.tensor(next(p_generator), dtype=torch.float)
+    q = torch.tensor(next(q_generator), dtype=torch.float)
+
+    return model(Variable(q))
+
+
+###############################################################################
+#
+# Q1.3: training 21 models 
+#
+###############################################################################
+if __name__ == '__main__':
+    phi = np.arange(-1,1.1,step=0.1)
+    uniformsJSD = []
+    uniformsWD = []
+    Dy = torch.zeros((100,21))
+    #Dy = []
+    #Usining Jensen-shannon loss
+    for i in range(21):
+        D = Discriminator(input_size=2, hidden_size=64)
+        D = train(model=D, loss_fn=JSD_loss_function, num_epochs=6000, batch_size=512, phi=phi[i])
+        Dy[:, i:i+1] = eval(D, batch_size=100, phi=phi[i])
+
+    for i in range(21):
+        uniformsJSD.append(np.log(2) - JSD_loss_function(Dy[:,10], Dy[:,i]))
+
+    plt.plot(phi, uniformsJSD)
+    plt.show()
+    input()
+    plt.close()
+
+
+    
+
+    ###############################################################################
+    #
+    # Q1.4: 
+    #
+    ###############################################################################
